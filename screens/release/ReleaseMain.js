186e699207254987ea71de13c6c80ce0c527d59e
Przemyslaw <przemyslaw.kmiecik@etendard.pl>
2021-01-29 21:29:36 +0100

    ReleaseMain: in-line styling moved to styles constant

--- screens/release/ReleaseMain.js
+++ screens/release/ReleaseMain.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet,ImageBackground } from 'react-native';
 
 import ButtonWithContentComponent from '../../components/ButtonWithContentComponent'
 
@@ -8,9 +8,9 @@ const ReleaseMain = (props) => {
   const image = require('../../assets/bg-lake.jpg');
 
   return (
-    <ImageBackground source={image} style={{ width: '100%', flex: 1, alignItems: 'stretch', alignContent: 'stretch' }}>
-      <View style={{ paddingTop: 50, minHeight: 100, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 24, color: 'white' }}>Release It</Text>
+    <ImageBackground source={image} style={styles.releaseImageBackground}>
+      <View style={styles.titleViewStyle}>
+        <Text style={styles.titleTextStyle}>Release It</Text>
       </View>
       <ButtonWithContentComponent title='Bioenergotherapic Excercise' content='
         CBT-style diary to record the causes and consequences of episodes of anger. You are encouraged to record
@@ -18,13 +18,27 @@ const ReleaseMain = (props) => {
       ' />
       <ButtonWithContentComponent title='Yoga for Anger' content='
       Change your mental pattern and you change your emotion'/>
-
-      {/* <Button
-          title="CalmDetails"
-          onPress={() => props.navigation.navigate('CalmDetails')}
-        /> */}
     </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  releaseImageBackground: {
+    width: '100%',
+    flex: 1,
+    alignItems: 'stretch',
+    alignContent: 'stretch'
+  },
+  titleViewStyle: {
+    paddingTop: 50,
+    minHeight: 100,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  titleTextStyle: {
+    fontSize: 24,
+    color: 'white'
+  }
+})
 
 export default ReleaseMain;
